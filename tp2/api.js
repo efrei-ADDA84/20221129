bf30845fc54cf698735668287705ef031cf35dec
tech <tech@MacBook-Pro-de-tech.local>
2024-04-12 15:19:21 +0200

    Editing files : change port from 8084 to 8084 and custom client answer

--- tp2/api.js
+++ tp2/api.js
@@ -5,20 +5,21 @@ const app = express();
 
 app.get("/", async (req, res) => {
   const { lat, lon } = req.query;
-  const API_KEY = process.env.API_KEY;
+  const API_KEY = "b498963e6b2e73ceaaaa40a6e510e032";
   const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=${API_KEY}`;
   try {
     const response = await fetch(weatherURL);
     const weatherData = await response.json();
-    console.log("temperature : ", weatherData.main.temp);
-    res.json(weatherData.main.temp);
+    const temperature = weatherData.main.temp;
+    console.log("> successful request");
+    res.status(200).json({ temperature: temperature });
   } catch (error) {
     console.error("Error fetching weather data:", error);
     res.status(500).json({ error: "Error fetching weather data" });
   }
 });
 
-const port = 8084;
+const port = 8081;
 app.listen(port, () => {
-  console.log("Api is running on port : ", port);
+  console.log("API is running on port:", port);
 });
